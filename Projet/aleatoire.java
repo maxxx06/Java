153f978398b05d25457b8d2eb8127d0e13b4989f
Maxime Lecomte <maxime.lecomte06@free.fr>
2019-02-07 11:13:22 +0100

    aleatoire Done!

--- Projet/aleatoire.java
+++ Projet/aleatoire.java
@@ -11,13 +11,63 @@ public class Aleatoire {
             int choix = saisie_entier();
             switch (choix) {
                 case 0: System.exit(0);
-                case 1: game();break;
+                // case 1: game();break;
                 case 2: aleatoire();break;
             }
 
         }
     }
 
+    public static void aleatoire() {
+        Vector tab = new Vector();
+        for ( int i=0;i<400;i++) {
+            tab.add(i);
+        }
+        Vector Cell_x = new Vector();
+        Vector Cell_y = new Vector();
+        Vector Cell_z = new Vector();
+        Vector Virus = new Vector();
+        for (int i = 0; i<33; i++ ) { // on le meme positionnement pour x,y,z
+            Random rand = new Random();
+            int indice= rand.nextInt(tab.size());
+            int new_indice_line=indice%20;
+            int new_indice_column=indice/20;
+            Cell_x.add("\n line = "+new_indice_line+" column = "+new_indice_column);
+            // Cell_z.add("\n line = "+new_indice_line+" column = "+new_indice_column);
+            tab.remove(i);
+        }
+        for (int i = 0; i<33; i++ ) { // on le meme positionnement pour x,y,z
+            Random rand = new Random();
+            int indice= rand.nextInt(tab.size());
+            int new_indice_line=indice%20;
+            int new_indice_column=indice/20;
+            Cell_y.add("\n line = "+new_indice_line+" column = "+new_indice_column);
+            tab.remove(i);
+        }
+
+        for (int i = 0; i<33; i++ ) { // on le meme positionnement pour x,y,z
+            Random rand = new Random();
+            int indice= rand.nextInt(tab.size());
+            int new_indice_line=indice%20;
+            int new_indice_column=indice/20;
+            Cell_z.add("\n line = "+new_indice_line+" column = "+new_indice_column);
+            tab.remove(i);
+        }
+
+        for (int i =0;i<10;i++) {
+            Random rand = new Random();
+            int indice= rand.nextInt(tab.size());
+            int new_indice_line=indice%20;
+            int new_indice_column=indice/20;
+            Virus.add("\n line = "+new_indice_line+" column = "+new_indice_column);
+            tab.remove(i);
+        }
+
+        System.out.println("\nCelle_x = "+Cell_x);
+        System.out.println("\nCell_y = "+Cell_y);
+        System.out.println("\nCell_z = "+Cell_z);
+        System.out.println("\nVirus = "+Virus);
+    }
 
 
 
